e58aca4a53cc3b1cd0dc18c82ddfde1dbe53beb6
Nick <nicholas.m.ricci@gmail.com>
2023-04-07 15:50:56 +0000

    No connections yet

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -19,13 +19,80 @@ flush privileges;
 use rps_league;
 
 -- Put your DDL 
-CREATE Table Players (
-    PlayerID int,
-    First_Name varchar(50),
-    Last_Name varchar(50),
-    Birthday date,
+CREATE TABLE Players (
+    playerID int,
+    firstName varchar(50),
+    lastName varchar(50),
+    birthday date,
     joinDate date,
     playerStatus varchar(50),
     phoneNumber varchar(15),
-    teamName varchar(50),
+    teamName varchar(50)
 );
+
+CREATE TABLE Teams (
+    teamName varchar(50),
+    joinDate date,
+    streetAddress varchar(50),
+    city varchar(50),
+    stateAbbrev varchar(2),
+    zipCode varchar(50)
+);
+
+CREATE TABLE Spectators (userName varchar(50), pwd varchar(50));
+
+CREATE TABLE SpectatorFavPlayers (userName varchar(50), playerID int);
+
+CREATE TABLE SpectatorFavTeams (userName varchar(50), teamName varchar(50));
+
+CREATE TABLE Coaches (
+    coachID int,
+    firstName varchar(50),
+    lastName varchar(50),
+    joinDate date,
+    birthday date,
+    phoneNumber varchar(15)
+);
+
+CREATE TABLE Posts (
+    postID int,
+    userName varchar(50),
+    content text,
+    timePosted datetime
+);
+
+CREATE TABLE Comments (
+    commentID int,
+    postID int,
+    userName varchar(50),
+    content text,
+    timePosted datetime
+);
+
+CREATE TABLE Likes (userName varchar(50), postID int);
+
+CREATE TABLE GamesPlayed(
+    gameID int,
+    player1ID int,
+    player2ID int,
+    team1 varchar(50),
+    team2 varchar(50),
+    player1Throw varchar(8),
+    player2Throw varchar(8),
+    tournament int,
+);
+
+CREATE TABLE Tournament(
+    tournamentID int,
+    name varchar(50),
+    attendance int,
+    streetAddress varchar(50),
+    city varchar(50),
+    stateAbbrev varchar(2),
+    zipCode varchar(50),
+    startDate date,
+    endDate date,
+    season year,
+);
+
+CREATE TABLE Season(yearPlayed year);

f28f25ea05aa4ba30c473ce66736b086cde7ae77
Nick <nicholas.m.ricci@gmail.com>
2023-04-07 03:43:24 +0000

    bootstrap started

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -1,5 +1,4 @@
 -- This file is to bootstrap a database for the CS3200 project. 
-
 -- Create a new database.  You can change the name later.  You'll
 -- need this name in the FLASK API file(s),  the AppSmith 
 -- data source creation.
@@ -10,7 +9,8 @@ create database rps_league;
 -- all privilages to the new database we just created. 
 -- TODO: If you changed the name of the database above, you need 
 -- to change it here too.
-grant all privileges on rps_league.* to 'webapp'@'%';
+grant all privileges on rps_league.* to 'webapp' @'%';
+
 flush privileges;
 
 -- Move into the database we just created.
@@ -19,3 +19,13 @@ flush privileges;
 use rps_league;
 
 -- Put your DDL 
+CREATE Table Players (
+    PlayerID int,
+    First_Name varchar(50),
+    Last_Name varchar(50),
+    Birthday date,
+    joinDate date,
+    playerStatus varchar(50),
+    phoneNumber varchar(15),
+    teamName varchar(50),
+);
